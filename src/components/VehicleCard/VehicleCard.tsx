6e4057904710976220681b731492f4457d48fcf5
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-03-07 22:54:36 +0100

    style: Vehicle card, simple layout

--- src/components/VehicleCard/VehicleCard.tsx
+++ src/components/VehicleCard/VehicleCard.tsx
@@ -4,6 +4,10 @@ import Link from "next/link";
 import { VisuallyHidden } from "@/components/VisuallyHidden";
 import type { Car } from "@/lib/types";
 
+// import ChevronSmall from "@/icons/chevron-small.svg";
+
+import style from "./style.module.css";
+
 export const VehicleCard = ({
   id,
   modelName,
@@ -11,38 +15,48 @@ export const VehicleCard = ({
   modelType,
   imageUrl,
 }: Car) => (
-  <article aria-label={`${modelName}`} role="contentinfo">
-    <header>
-      <span>{bodyType}</span>
+  <article
+    aria-label={`${modelName}`}
+    role="contentinfo"
+    className={style.cardWrapper}
+  >
+    <header className={style.cardHeader}>
+      <span className={style.cardSubTitle}>{bodyType}</span>
 
-      <h2>{modelName}</h2>
-
-      <span>{modelType}</span>
+      <div>
+        <h2 className={style.cardTitle}>{modelName}</h2>{" "}
+        <span>{modelType}</span>
+      </div>
     </header>
 
-    <section aria-label={modelName}>
-      <div>
-        <Image
-          src={imageUrl}
-          alt={`An image of ${modelName} car model`}
-          width={800}
-          height={600}
-          style={{ objectFit: "contain", width: "100%", height: "auto" }}
-        />
-      </div>
+    <section aria-label={modelName} className={style.cardContent}>
+      <Image
+        className={style.cardImage}
+        src={imageUrl}
+        alt={`An image of ${modelName} car model`}
+        width={800}
+        height={600}
+        style={{ objectFit: "contain", width: "100%", height: "auto" }}
+      />
     </section>
 
-    <footer role="contentinfo">
-      <VisuallyHidden>More about {modelName}</VisuallyHidden>
+    <footer role="contentinfo" className={style.cardFooter}>
+      <VisuallyHidden>
+        More about {modelName}, {modelType}
+      </VisuallyHidden>
 
-      <Link href={`/learn/${id}`}>
-        <VisuallyHidden>Read more about {modelName}</VisuallyHidden>
-        Learn
+      <Link href={`/learn/${id}`} className={style.cardCallToAction}>
+        <VisuallyHidden>
+          Read more about {modelName}, {modelType}
+        </VisuallyHidden>
+        <span aria-hidden="true">Learn</span>
       </Link>
 
-      <Link href={`/shop/${id}`}>
-        <VisuallyHidden>Purchase {modelName}</VisuallyHidden>
-        Shop
+      <Link href={`/shop/${id}`} className={style.cardCallToAction}>
+        <VisuallyHidden>
+          Purchase {modelName}, {modelType}
+        </VisuallyHidden>
+        <span aria-hidden="true">Shop</span>
       </Link>
     </footer>
   </article>
